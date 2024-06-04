7452b1bd7c9c8b809a4f1c32bce7c82561dd04a5
lucanmu <lucanmu@hotmail.com>
2024-06-04 15:50:23 -0300

    z

--- src/app/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import authReducer from "../component/Pages/SignUp/authSlice.js"
-
-
-export const store = configureStore({
-    reducer: {
-        auth: authReducer
-    },
-})
